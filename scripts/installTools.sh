71764f9df6bac94564983ccdc29c646bffc94b57
Markus Straußberger <mail@marstra.de>
2019-10-13 01:00:24 +0200

    added emcas and htop to the list of tools to install

--- scripts/installTools.sh
+++ scripts/installTools.sh
@@ -2,5 +2,5 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt -y install git build-essential vim tmux xclip zsh terminator ranger
+sudo apt -y install git build-essential emacs vim tmux xclip zsh terminator ranger htop
 sudo apt -y install i3 chromium-browser
