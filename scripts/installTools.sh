201371346d8db9999745b8baf8e644ca0fa51484
Markus Straußberger <mail@marstra.de>
2019-10-16 22:42:21 +0200

    install unzip

--- scripts/installTools.sh
+++ scripts/installTools.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt -y install git curl wget
+sudo apt -y install git curl wget unzip
 sudo apt -y install build-essential emacs vim tmux xclip zsh terminator ranger htop
 # sudo apt -y install i3 chromium-browser
 
