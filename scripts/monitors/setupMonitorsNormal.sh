8acf72fc87880bb5ffdf48bec737649ebe6e929b
Markus Straußberger <mail@marstra.de>
2019-10-26 18:59:56 +0200

    removed tv

--- scripts/monitors/setupMonitorsNormal.sh
+++ scripts/monitors/setupMonitorsNormal.sh
@@ -3,4 +3,4 @@
 # DVI-I-1 => Right Monitor
 # DVI-D-0 => Middle Monitor
 # DP-0 => Left Monitor
-xrandr --output DVI-I-1 --pos 3840x0 --rotate normal --output DVI-D-0 --pos 1920x0 --rotate normal --output DP-0 --pos 0x0 --primary --rotate normal --output HDMI-0 --pos 0x0 --rotate normal --mode 1920x1080
+xrandr --output DVI-I-1 --pos 3840x0 --rotate normal --output DVI-D-0 --pos 1920x0 --rotate normal --output DP-0 --pos 0x0 --primary --rotate normal # --output HDMI-0 --pos 0x0 --rotate normal --mode 1920x1080
