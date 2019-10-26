1a456e7bc946c59ab578a9e4c65985d22f7f8860
Markus Straußberger <mail@marstra.de>
2019-10-26 11:47:23 +0200

    start evolution on startup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -645,5 +645,7 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
 -- autostarts here
+-- TODO use something like https://zmalltalker.com/linux/awesome-autostart.html to ensure every program is just started once
+awful.util.spawn("evolution")
 awful.util.spawn("setupMonitorsNormal.sh")
 -- }}}
