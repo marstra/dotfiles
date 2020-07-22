5706c7ad0bc8ebd6febb84b38e3ae5d343123a95
Markus Straußberger <markus.straussberger@ds.mhie.com>
2020-07-22 18:13:05 +0200

    updated installTools
    - new ubuntu version 20.04
    - install new package versions

--- scripts/installTools.sh
+++ scripts/installTools.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt -y install git curl wget unzip build-essential net-tools
-sudo apt -y install emacs vim-gnome tmux ranger htop
+sudo apt -y install emacs vim tmux ranger htop
 
 # TODO gui related stuff only when parameter gui is passed
 sudo apt -y install chromium-browser xclip terminator pcmanfm
@@ -27,30 +27,22 @@ function installNodeJS() {
 }
 
 function installDotnetCore() {
-    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
 
-    sudo add-apt-repository universe
     sudo apt update
     sudo apt install -y apt-transport-https
     sudo apt update
-    sudo apt install -y dotnet-sdk-3.0
+    sudo apt install -y dotnet-sdk-3.1
 }
 
 function installDocker() {
     # see: https://docs.docker.com/install/linux/docker-ce/ubuntu/
-    sudo apt -y install \
-	 apt-transport-https \
-	 ca-certificates \
-	 gnupg-agent \
-	 software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-	 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	   $(lsb_release -cs) \
-	   stable"
-    sudo apt update
-    sudo apt -y install docker-ce docker-ce-cli containerd.io
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    rm get-docker.sh
+
     sudo usermod -aG docker $USER
 }
 
