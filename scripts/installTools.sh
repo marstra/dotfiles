42087522f8bd7403906e01be1b8699966fc4072a
Markus Straußberger <markus.straussberger@ds.mhie.com>
2020-07-23 10:22:44 +0200

    updated install-tools to install docker-compose

--- scripts/installTools.sh
+++ scripts/installTools.sh
@@ -46,6 +46,12 @@ function installDocker() {
     sudo usermod -aG docker $USER
 }
 
+function installDockerCompose() {
+    # see: https://docs.docker.com/install/linux/docker-ce/ubuntu/
+    sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
+
 function installReversing() {
     # install radare
     sudo mkdir /opt/radare2
@@ -58,6 +64,7 @@ function installReversing() {
 
 installZsh
 installDocker
+installDockerCompose
 installNodeJS
 installDotnetCore
 
