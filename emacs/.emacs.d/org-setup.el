f42f60341c6c6a2cd40de420bbb2406eb11c1fe1
Markus Straußberger <mail@marstra.de>
2021-01-09 09:48:27 +0100

    config

--- emacs/.emacs.d/org-setup.el
+++ emacs/.emacs.d/org-setup.el
@@ -1,3 +1,3 @@
 ;; open notes using f2
-(global-set-key (kbd "<f2>") (lambda () (interactive) (find-file "~/OneDrive/notes/notes.org")))
-(setq org-agenda-files (list "~/OneDrive/notes/notes.org"))
+(global-set-key (kbd "<f2>") (lambda () (interactive) (find-file "~/repos/notes/Inbox.org")))
+(setq org-agenda-files (list "~/repos/notes/Inbox.org"))
