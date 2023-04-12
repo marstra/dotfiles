48d7081e7b805db19ed50cbff6f10d49763466fd
Markus Straußberger <mail@marstra.de>
2023-04-12 16:46:26 +0200

    new version of fonts

--- scripts/installFonts.sh
+++ scripts/installFonts.sh
@@ -3,7 +3,7 @@ set -e
 tmpDir=$(mktemp -d)
 cd $tmpDir
 # install nerd-font hack
-wget -O hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
+wget -O hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip
 unzip hack.zip -d hack
 sudo mkdir /usr/share/fonts/truetype/hack
 sudo mv hack/*.ttf /usr/share/fonts/truetype/hack
