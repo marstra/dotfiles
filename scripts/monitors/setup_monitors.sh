03121f49f17eccef69a3b71101923f37bc92b99f
Markus Straußberger <mail@marstra.de>
2024-03-27 11:58:39 +0100

    removed monitor script

--- scripts/monitors/setup_monitors.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# DVI-D-0 => Right Monitor
-# HDMI-0 => Middle Monitor
-# DP-3 => Left Monitor
-xrandr --output DP-1 --pos 0x0 --rotate normal --output HDMI-0 --pos 2560x0 --rotate normal --primary --output HDMI-1 --pos 5120x0 --rotate normal
