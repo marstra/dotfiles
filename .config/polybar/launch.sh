5489ff79be59b5dfb4747cc2b3bfb4742711778d
Markus Straußberger <mail@marstra.de>
2024-11-25 14:38:42 +0100

    update polybar launch script to use polybar for monitor detection instead of xrandr

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,8 @@
 polybar-msg cmd quit
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    export MONITOR=$m
+  for MONITOR in $(polybar -m | cut -d":" -f1); do
+    export MONITOR
     echo "start polybar on $MONITOR"
     polybar --reload $CONFIG mybar 2>&1 | tee -a /tmp/polybar_monitor_${MONITOR}.log & disown
   done
