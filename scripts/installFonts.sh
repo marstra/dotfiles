a739bc3fb7c73781dd6858f4812878470f11d809
Markus Straußberger <mail@marstra.de>
2025-01-01 19:37:56 +0100

    updated install fonts script

--- scripts/installFonts.sh
+++ scripts/installFonts.sh
@@ -3,9 +3,9 @@ set -e
 tmpDir=$(mktemp -d)
 cd $tmpDir
 # install nerd-font hack
-wget -O hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip
+wget -O hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Hack.zip
 unzip hack.zip -d hack
-sudo mkdir /usr/share/fonts/truetype/hack
+sudo mkdir -p /usr/share/fonts/truetype/hack
 sudo mv hack/*.ttf /usr/share/fonts/truetype/hack
 cd ~
 rm -rf $tmpDir
