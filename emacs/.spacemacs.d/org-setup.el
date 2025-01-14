7f2fbfd2e7816d2062ece291de40c3ec5341d3b7
Markus Straußberger <mail@marstra.de>
2025-01-14 23:17:11 +0100

    setup org-capture and org-refile

--- emacs/.spacemacs.d/org-setup.el
+++ emacs/.spacemacs.d/org-setup.el
@@ -1,2 +1,9 @@
 ;; Set the directory for org-agenda files
 (setq org-agenda-files '("~/repos/pers/notes"))
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 1))))
+(setq org-default-notes-file "~/repos/pers/notes/Inbox.org")
+
+(setq org-capture-templates
+      '(("s" "Schedule task to inbox" entry
+         (file+headline "~/repos/pers/notes/Inbox.org" "Inbox")
+         "* TODO %?\12SCHEDULED: %T" :jump-to-captured t)))
