96119127c4d4924d119a79a2ff73ecb9dd2849e2
Markus Straußberger <mail@marstra.de>
2025-01-19 09:09:57 +0100

    restructuring of notes

--- emacs/.spacemacs.d/org-setup.el
+++ emacs/.spacemacs.d/org-setup.el
@@ -1,11 +1,11 @@
 ;; Set the directory for org-agenda files
-(setq org-agenda-files '("~/repos/pers/notes"))
+(setq org-agenda-files '("~/repos/pers/notes/org"))
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 1))))
-(setq org-default-notes-file "~/repos/pers/notes/Inbox.org")
+(setq org-default-notes-file "~/repos/pers/notes/org/inbox.org")
 
 (setq org-capture-templates
       '(("s" "Schedule task to inbox" entry
-         (file+headline "~/repos/pers/notes/Inbox.org" "Inbox")
+         (file+headline "~/repos/pers/notes/org/inbox.org" "Inbox")
          "* TODO [#3] %?\12SCHEDULED: %T" :jump-to-captured nil)))
 
 (setq org-capture-use-agenda-date t)
