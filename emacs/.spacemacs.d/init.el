ff6bc6b32755bd55db894761e14def8a9ce13cdd
Markus Straußberger <mail@marstra.de>
2025-01-23 22:36:07 +0100

    setup org mode effort stuff

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -32,7 +32,8 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(yaml
+   '(csv
+     yaml
      python
      (csharp :variables csharp-backend 'omnisharp)
      (c-c++ :variables c-c++-backend 'lsp-clangd)
--- emacs/.spacemacs.d/org-setup.el
+++ emacs/.spacemacs.d/org-setup.el
@@ -1,12 +1,17 @@
 ;; Set the directory for org-agenda files
 (setq org-agenda-files '("~/repos/pers/notes/org"))
-(setq org-refile-targets '((org-agenda-files . (:maxlevel . 1))))
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 2))))
 (setq org-default-notes-file "~/repos/pers/notes/org/inbox.org")
 
 (setq org-capture-templates
       '(("s" "Schedule task to inbox" entry
          (file+headline "~/repos/pers/notes/org/inbox.org" "Inbox")
          "* TODO [#3] %?\12SCHEDULED: %T" :jump-to-captured nil)))
+
+(setq org-global-properties
+      '(("Effort_ALL" . "0:05 0:10 0:15 0:30 0:45 1:00 1:30 2:00 3:00 4:00 5:00 6:00 7:00")))
+(setq org-columns-default-format
+      "%40ITEM(Task) %17Effort(Estimated Effort){:} %10CLOCKSUM_T(Time Spent)")
 
 (setq org-capture-use-agenda-date t)
 (setq org-agenda-show-future-repeats nil)
